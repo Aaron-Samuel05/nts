b4c168a1ade10503520f0661b166d4ed1e13a3b1
Aaron-Samuel05 <157136322+Aaron-Samuel05@users.noreply.github.com>
2026-09-16 15:51:37 +0530

    Enable native-feeling mobile spirit swipe

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './spirit-mobile-fix.css'
+import './spirit-native-swipe.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
