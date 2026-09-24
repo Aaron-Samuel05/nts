72715d3fc1b7f5d5908b1c0d5a3ec6072e8524e1
Aaron-Samuel05 <157136322+Aaron-Samuel05@users.noreply.github.com>
2026-09-24 11:55:41 +0530

    Use supplied hero image

--- src/components/BannerSection.jsx
+++ src/components/BannerSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const heroImage = '/banner/east-coast-bottle-lineup-hero-desktop.png'
+const heroImage = '/banner/east-coast-bottle-lineup-hero.jpg'
 
 export default function BannerSection() {
   return (
